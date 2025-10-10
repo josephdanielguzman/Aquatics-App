ffdf10e253c0687325e330bec07610cf7a88f51f
joseph <Joseph.Guzman01@student.csulb.edu>
2025-10-09 23:15:36 -0700

    feat: rotation card data now expandable and collapsible
    Signed-off-by: joseph <Joseph.Guzman01@student.csulb.edu>

--- lifeguard_app/frontend/src/components/Rotations/GreenRotation.jsx
+++ lifeguard_app/frontend/src/components/Rotations/GreenRotation.jsx
@@ -1,20 +1,40 @@
 import {Button, TimePicker} from "antd";
-import {HistoryOutlined, ReloadOutlined} from "@ant-design/icons";
+import {DownOutlined, HistoryOutlined, ReloadOutlined} from "@ant-design/icons";
+import { useState } from "react";
 
 export default function GreenRotation() {
     const format = 'h:mm A'
+    const [showData, setShowData] = useState(false);
+
+    const handleClick = () => {
+        setShowData(!showData);
+    }
     return(
-        <div className={'rounded-md bg-white h-full shadow'}>
+        <div className={'mt-7 min-w-fit'}>
             <div className={'bg-green-500 border-green-600 border-1 rounded-t-md text-white font-sans p-3 pb-2'}>
-                <div className={'flex justify-between'}>
-                    <p className={'text-white font-sans font-bold text-xl'}>Green</p>
-                    <div className={'flex gap-0.5'}>
-                        <TimePicker format={format} />
-                        <Button variant={'outlined'} color={'green'} icon={<ReloadOutlined />}>Rotate</Button>
+                <div className={'flex gap-x-3'}>
+                    <div className={'flex items-center pb-2'}>
+                      <Button onClick={handleClick} variant={'outlined'} color={'green'} icon={<DownOutlined/>}></Button>
+                    </div>
+                    <div className={'flex justify-between w-full'}>
+                        <div>
+                            <p className={'text-white font-sans font-bold text-xl'}>Green</p>
+                            <p><HistoryOutlined /> Last Rotated: 10:50</p>
+                        </div>
+                        <div className={'flex gap-0.5 h-min'}>
+                            <TimePicker format={format} />
+                            <Button variant={'outlined'} color={'green'} icon={<ReloadOutlined />}>Rotate</Button>
+                        </div>
                     </div>
                 </div>
-                <p><HistoryOutlined /> Last Rotated: 10:50</p>
             </div>
+            {showData && (
+                <div className={'rounded-b-md bg-white h-full shadow'}>
+                    <p>
+                        print this
+                    </p>
+                </div>
+            )}
         </div>
     )
 }
--- lifeguard_app/frontend/src/components/Rotations/VioletRotation.jsx
+++ lifeguard_app/frontend/src/components/Rotations/VioletRotation.jsx
@@ -1,20 +1,41 @@
 import {Button, TimePicker} from "antd";
-import {ReloadOutlined, HistoryOutlined} from "@ant-design/icons";
+import {ReloadOutlined, HistoryOutlined, DownOutlined} from "@ant-design/icons";
+import {useState} from "react";
 
 export default function VioletRotation() {
     const format = 'h:mm A'
+    const [showData, setShowData] = useState(false)
+
+    const handleClick = () => {
+        setShowData(!showData)
+    }
+
     return (
-        <div className={'rounded-md bg-white h-full shadow'}>
+        <div>
             <div className={'bg-pink-400 border-pink-500 border-1 rounded-t-md text-white font-sans p-3 pb-2'}>
-                <div className={'flex justify-between'}>
-                    <p className={'text-white font-sans font-bold text-xl'}>Violet</p>
-                    <div className={'flex gap-0.5'}>
-                        <TimePicker format={format} />
-                        <Button variant={'outlined'} color={'pink'} icon={<ReloadOutlined />}>Rotate</Button>
+                <div className={'flex gap-x-3'}>
+                    <div className={'flex items-center pb-2'}>
+                      <Button onClick={handleClick} variant={'outlined'} color={'pink'} icon={<DownOutlined/>}></Button>
+                    </div>
+                    <div className={'flex justify-between w-full'}>
+                        <div>
+                            <p className={'text-white font-sans font-bold text-xl'}>Violet</p>
+                            <p><HistoryOutlined /> Last Rotated: 10:50</p>
+                        </div>
+                        <div className={'flex gap-0.5 h-min'}>
+                            <TimePicker format={format} />
+                            <Button variant={'outlined'} color={'pink'} icon={<ReloadOutlined />}>Rotate</Button>
+                        </div>
                     </div>
                 </div>
-                <p><HistoryOutlined /> Last Rotated: 10:50</p>
             </div>
+            {showData && (
+                <div className={'rounded-b-md bg-white h-auto shadow'}>
+                    <p>
+                        print this
+                    </p>
+                </div>
+            )}
         </div>
     )
 }
