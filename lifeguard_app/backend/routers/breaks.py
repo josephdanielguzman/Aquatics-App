38bf04f3104aa22abd5fd9205ddf54939a2cb47b
joseph <Joseph.Guzman01@student.csulb.edu>
2025-10-20 22:21:27 -0700

    feat: update breaks endpoints validation
    - Modified `start_break` to return `BreakResponse` schema
    - Updated `end_break` to include validation for minimum duration
    
    Signed-off-by: joseph <Joseph.Guzman01@student.csulb.edu>

--- lifeguard_app/backend/routers/breaks.py
+++ lifeguard_app/backend/routers/breaks.py
@@ -2,12 +2,13 @@ from lifeguard_app.backend import schemas, models
 from fastapi import HTTPException, Depends, APIRouter, status
 from sqlalchemy.orm import Session
 from lifeguard_app.backend.db import get_db
+from datetime import datetime, time, timedelta
 
 #TODO: finish breaks endpoint
 
 router = APIRouter(prefix="/breaks", tags=["breaks"])
 
-@router.post("/", response_model=schemas.BreakStart, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=schemas.BreakResponse, status_code=status.HTTP_201_CREATED)
 def start_break(new_break: schemas.BreakStart, db: Session = Depends(get_db)):
     new_break = models.Breaks(**new_break.dict())
     db.add(new_break)
@@ -15,14 +16,29 @@ def start_break(new_break: schemas.BreakStart, db: Session = Depends(get_db)):
     db.refresh(new_break)
     return new_break
 
-@router.post("/{id}:end-break", response_model=schemas.BreakResponse)
+@router.patch("/end_break/{id}", response_model=schemas.BreakResponse)
 def end_break(id: int, break_end: schemas.BreakEnd, db: Session = Depends(get_db)):
-    update_break = db.query(models.Breaks).filter(models.Breaks.id == id).first()
+    """ Updates the end time of an existing break given the id. """
+    # break to be updated
+    update_break = db.query(models.Breaks).filter(models.Breaks.id == id, models.Breaks.end_time.is_(None)).first()
+
+    # break times conversion
+    start_datetime = datetime.combine(datetime.today(), update_break.start_time )
+    end_datetime = datetime.combine(datetime.today(), break_end.end_time)
+
+    duration_datetime = (end_datetime - start_datetime).total_seconds() / 60
+
+    # invalid break
+    if duration_datetime < 10:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Break less than 10 minutes")
 
     if not update_break:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Break not found")
 
-    update_break.end_time = break_end.end_time
+    # update break data
+    for key, value in break_end.dict(exclude_unset=True).items():
+        setattr(update_break, key, value)
+
     db.commit()
     db.refresh(update_break)
     return update_break
