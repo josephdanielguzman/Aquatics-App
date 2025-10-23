e78fc70b0f85e101b6b57a50df0663af3a974a6f
joseph <Joseph.Guzman01@student.csulb.edu>
2025-10-23 14:16:30 -0700

    feat: add validation to prevent double spot assignment
    - Modified assignments endpoint to prevent double assignment
    
    Signed-off-by: joseph <Joseph.Guzman01@student.csulb.edu>

--- lifeguard_app/backend/routers/assignments.py
+++ lifeguard_app/backend/routers/assignments.py
@@ -1,5 +1,5 @@
 from lifeguard_app.backend import schemas, models
-from fastapi import Depends, status, APIRouter
+from fastapi import Depends, status, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 from lifeguard_app.backend.db import get_db
 
@@ -7,8 +7,33 @@ router = APIRouter(prefix="/assignments", tags = ["assignments"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Assignment)
 def create_assignment(assignment: schemas.Assignment, db: Session = Depends(get_db)):
+
+    # -- Error handling --
+
+    # Prevents double assignment
+    # Find most recent assignment of spot
+    latest_spot_assignment = (db.query(models.Assignments)
+                              .filter(models.Assignments.spot_id == assignment.spot_id)
+                              .order_by(models.Assignments.time.desc())
+                              .first())
+
+    # todo: only accounts for current day, needs modification for mult shifts
+    if latest_spot_assignment:
+        # Find most recent assignment of that shift
+        latest_assignment = (db.query(models.Assignments)
+                             .filter(models.Assignments.shift_id == latest_spot_assignment.shift_id)
+                             .order_by(models.Assignments.time.desc())
+                             .first())
+
+        # If the most recent assignment for the shift is the spot attempting to assign to, raise error
+        if latest_spot_assignment.spot_id == latest_assignment.spot_id:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Spot currently occupied")
+
+    # -- Create and persist new record --
+
     new_assignment = models.Assignments(**assignment.dict())
     db.add(new_assignment)
     db.commit()
     db.refresh(new_assignment)
+
     return new_assignment
