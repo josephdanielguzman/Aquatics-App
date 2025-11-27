e05fffb2dbf48d1e43065e91726aa8c4da942938
joseph <Joseph.Guzman01@student.csulb.edu>
2025-11-26 18:02:18 -0800

    fix: navigation logo
    - Logo placeholder removed, replaced with text
    
    Signed-off-by: joseph <Joseph.Guzman01@student.csulb.edu>

--- lifeguard_app/frontend/src/components/SideNav.jsx
+++ lifeguard_app/frontend/src/components/SideNav.jsx
@@ -6,8 +6,9 @@ import {HomeOutlined, ReloadOutlined, AuditOutlined} from '@ant-design/icons'
 export default function Sidebar() {
     return (
         <Menu defaultSelectedKeys={'1'} theme='dark' className={"text-center"}>
-            <div className={'bg-black h-25 w-25'}>
-                <p className={"pt-10"}>placeholder</p>
+            <div className={'flex flex-col justify-items-center h-20 mt-2 text-left pl-3 text-2xl font-semibold'}>
+                <p>Shift</p>
+                <p>Manager</p>
             </div>
             <Menu.Item key={'1'} icon={<HomeOutlined />}><NavLink to={'/'}>Home</NavLink></Menu.Item>
             <Menu.Item key={'2'} icon={<ReloadOutlined />}><NavLink to={'/Rotations'}>Rotations</NavLink></Menu.Item>
