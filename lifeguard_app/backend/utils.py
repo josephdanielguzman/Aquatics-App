78b8d1d1b6460784fa27ac950a20d7a2d281f60c
joseph <Joseph.Guzman01@student.csulb.edu>
2025-12-03 10:34:00 -0800

    feat: add endpoint for user registration and password hasher - Validation for empty usernames/passwords and existing usernames - Helper function to hash and verify passwords
    Signed-off-by: joseph <Joseph.Guzman01@student.csulb.edu>

---
+++ lifeguard_app/backend/utils.py
@@ -0,0 +1,10 @@
+from passlib.context import CryptContext
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hashify(password: str):
+    """Returns hashed password."""
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    """Returns True if password matches hashed password."""
+    return pwd_context.verify(plain_password, hashed_password)
