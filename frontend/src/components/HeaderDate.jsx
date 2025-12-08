712a5822ed91a7dbcd344544fde906d422c70ed1
joseph <Joseph.Guzman01@student.csulb.edu>
2025-12-08 10:12:49 -0800

    feat: add live time and date to HeaderDate.jsx
    Signed-off-by: joseph <Joseph.Guzman01@student.csulb.edu>

--- frontend/src/components/HeaderDate.jsx
+++ frontend/src/components/HeaderDate.jsx
@@ -1,14 +1,21 @@
+import { useState, useEffect } from 'react'
 
 export default function HeaderDate() {
-    const time = new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: true});
+
+    const[time, setTime] = useState(new Date())
+
+    useEffect(() => {
+        setInterval(() => setTime(new Date()), 1000)
+    },[])
+
     return (
         <div className={"flex justify-between font-sans font-semibold text-blue-950"}>
             <div >
-                <p className={"text-2xl"}>Monday</p>
-                <p className={'font-normal'}>September 2, 2025</p>
+                <p className={"text-2xl"}>{time.toLocaleDateString('en-US', {weekday: 'long'})}</p>
+                <p className={'font-normal'}>{time.toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'})}</p>
             </div>
             <div className={"text-2xl"}>
-                {time}
+                {time.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'})}
             </div>
         </div>
     )
