c91df9af51883b6027053fb5c785ccfa186f0626
joseph <Joseph.Guzman01@student.csulb.edu>
2025-12-12 19:58:39 -0800

    fix(front): add trailing slash to match api endpoint
    Signed-off-by: joseph <Joseph.Guzman01@student.csulb.edu>

--- frontend/src/api/rotations.js
+++ frontend/src/api/rotations.js
@@ -1,7 +1,7 @@
 import { api } from '/src/api/axios.js'
 
 export const getRotations = async () => {
-    const { data } = await api.get('/rotations')
+    const { data } = await api.get('/rotations/')
     return data
 }
 
