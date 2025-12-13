3339f56c91e26d9817fb590eb1535f011ff8e12c
joseph <Joseph.Guzman01@student.csulb.edu>
2025-12-12 19:21:29 -0800

    fix(back): add options and expose headers to fix CORS error
    Signed-off-by: joseph <Joseph.Guzman01@student.csulb.edu>

--- backend/lifeguardapi.py
+++ backend/lifeguardapi.py
@@ -12,7 +12,6 @@ app = FastAPI()
 
 origins = [
     "https://guard-sync.vercel.app",
-    "https://*.vercel.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
@@ -23,7 +22,9 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
+
 app.include_router(guards.router)
 app.include_router(assignments.router)
 app.include_router(shifts.router)
@@ -34,3 +35,7 @@ app.include_router(user.router)
 @app.get("/")
 async def root():
     return {"Message": "GG Aquatics"}
+
+@app.options("/{full_path:path}")
+async def options_handler():
+    return {}
